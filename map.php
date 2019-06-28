ded841dd860c186e5c84e2409d973fe175353066
Rodrigo Alexander Becerra Lucano <51519114+RAlexander777@users.noreply.github.com>
2019-06-28 08:58:21 -0500

    Add files via upload

--- map.php
+++ map.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <html>
 <head>
 
@@ -27,15 +27,16 @@
 <center>
 <form name="form1"  action="calPath.php" method="post" >
 
-De :
+Punto Inicial :
 <input type="text" size ="3" name="fromClass"/>
 
-Hacia :
+Punto Final :
 <input type="text" size= "3" name="toClass"/>
 <input name=b1 type=submit value="Calcular">
 
 </form>
 	
+
 
 </center>
 
@@ -44,56 +45,4 @@ Hacia :
 
 </body>
 </html>
-=======
-<html>
-<head>
-<title>Algoritmo Dijkstra</title>
 
-<script>
-
-function clickMap(classID)
-{
-    if (document.form1.fromClass.value.length == 0)
-    {
-        document.form1.fromClass.value = classID;
-    }
-    else
-    {
-        document.form1.toClass.value = classID;
-    }
-}
-
-</script>
-</head>
-<body>
-<header>
-<center>
-	<h1>
-	<br>
-		<strong>
-		Algoritmo del camino mas corto (Dijkstra)
-		</strong>
-	</br>
-	</h1>
-</center>
-</header> 
-<center><img src="Grafo.jpg"  usemap="#routes" border="0">
-</center>
-
-<center>
-<form name="form1"  action="calPath.php" method="post" >
-
-De :
-<input type="text" size ="3" name="fromClass"/>
-
-Hacia :
-<input type="text" size= "3" name="toClass"/>
-<input name=b1 type=submit value="Calcular">
-
-</form>
-	
-
-</center>
-
-<br></body></html>
->>>>>>> f07f67a99b1ce3bfbd69f6dee1a4a0d42e789a18
